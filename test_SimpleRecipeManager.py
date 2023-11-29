45f150d3e0e0b6d6a6252ee22995c197de66f214
Aamir1873 <149040255+Aamir1873@users.noreply.github.com>
2023-11-29 17:05:59 +0300

    added a add recipe test
    it ensures if the open form is called

--- test_SimpleRecipeManager.py
+++ test_SimpleRecipeManager.py
@@ -18,7 +18,19 @@ class TestRecipeGUI(unittest.TestCase):
     @patch('tkinter.Tk.mainloop')
     def test_app_doesnt_open(self, mock_mainloop):
         mock_mainloop.assert_not_called()
+    def test_add_recipe(self):
+            # Mocking the open_form method
+            with patch.object(self.app, 'open_form') as mock_open_form:
+                # Trigger the Add Recipe button
+                try:
+                    self.app.add_recipe()
+                except Exception as e:
+                    self.fail(f"Error encountered: {e}")
 
+                # Ensure the open_form method is called with the correct parameters
+                mock_open_form.assert_called_once_with()
+
+            
 
 if __name__ == '__main__':
     unittest.main()
